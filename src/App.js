a120c447f6f6cebda3b901a812f630f54fd6d934
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2020-12-29 19:42:24 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 import { ToastContainer } from 'react-toastify';
 import ImageGallery from './Components/ImageGallery/ImageGallery';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <ToastContainer autoClose={3000} />
-        <ImageGallery />
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div>
+      <ToastContainer autoClose={3000} />
+      <ImageGallery />
+    </div>
+  );
 }
-
-export default App;
